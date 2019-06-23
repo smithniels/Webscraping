592a861d7cb4fcf79e744ee27b35edbcc88369a8
Niels Smith <30351969+smithniels@users.noreply.github.com>
2019-06-23 09:43:15 -0400

    Update insta.py

--- insta.py
+++ insta.py
@@ -20,29 +20,29 @@ print('Module Import = Done \n \n Start the next bit') #\n will create a new lin
 
 #part ONE (makes hashtag + opens instagram )
 
-hashtag = 'Cal' # This assigns the #hashtag that will be searched for in Instagram
-print('Looking for #',hashtag+"!") # Variabales can be included in print functions like this. remember to seperate the pieces of your
+hashtag = 'SundayFunday' # This assigns the #hashtag that will be searched for in Instagram
+print('Looking for >#',hashtag+"<") # Variabales can be included in print functions like this. remember to seperate the pieces of your
                                    # Print function with commas , or plus + signs
-browser = webdriver.Chrome(r'C:\Users\19258\Desktop\chromedriver.exe') # Opens the webdriver that will open the corresponding browser
+browser = webdriver.Chrome(r'C:\Users\19258\Desktop\chromedriver.exe') # Finds the webdriver.exe on the path provided
                                                                        # Selenium requires a driver to interface with the chosen browser
                                                                        # All webdrivers can be found at: https://docs.seleniumhq.org/download/
-browser.get('https://www.instagram.com/explore/tags/' + hashtag) # Inputs the url with the hashtag 
+                                                                       # You will need to download the corresponding webdriver for the browser you want to use
+browser.get('https://www.instagram.com/explore/tags/' + hashtag) # Load the page at the given url with the #Hashtag
 print("Here is the browser",browser)
-Pagelength = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);") #scrolls to the ZERO position (horizontal)
-                                                                                       #document.body.scrollHeight gets the height
+Pagelength = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Scrolls to the ZERO position (horizontal)
+                                                                                       # Document.body.scrollHeight gets the height
 print('Part1 is complete')
 
 # part TWO (parse the html + adds ___ to links)
 
 links=[] #creates an empty list "links"
-source = browser.page_source
-data=bs(source, 'html.parser')
-body = data.find('body')
-script = body.find('span')
-for link in script.findAll('a'):
-     # print("This is link",link)
-     if re.match("/p", link.get('href')):
-        links.append('https://www.instagram.com'+link.get('href'))
+source = browser.page_source # Open the source page
+data = bs(source, 'html.parser') # Use beautiful soup to parse it.
+body = data.find('body')         # Search through BeautifulSoup file for <body> tags
+script = body.find('span')       # Within the <body> tags find the <span> tags
+for link in script.findAll('a'): # For the <span> tags, find the <a..>
+     if re.match("/p", link.get('href')):   # Searching for this format <a href="/p/BzDY-D-D8X3/"
+        links.append('https://www.instagram.com'+link.get('href')) # Takes the "/p/BzDY-D-D8X3/"
 result=pd.DataFrame()
 for i in range(len(links)):
     try:
@@ -53,7 +53,7 @@ for i in range(len(links)):
         raw = script.text.strip().replace('window._sharedData =', '').replace(';', '')
         json_data=json.loads(raw)
         posts =json_data['entry_data']['PostPage'][0]['graphql']
-        posts= json.dumps(posts)
+        posts = json.dumps(posts)
         posts = json.loads(posts)
         x = pd.DataFrame.from_dict(json_normalize(posts), orient='columns')
         x.columns =  x.columns.str.replace("shortcode_media.", "")
