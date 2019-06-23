429aa4f027578bfaf54427de8bbc4be0a8a9aca6
Niels Smith <30351969+smithniels@users.noreply.github.com>
2019-06-23 14:16:18 -0400

    Update insta.py

--- insta.py
+++ insta.py
@@ -13,21 +13,21 @@ import json #^
 from selenium import webdriver # Searches and loads webdriver in the selenium module
 from urllib.request import urlopen          #^
 from pandas.io.json import json_normalize   #^
-from bs4 import BeautifulSoup as bs # searches and loads BeautifulSoup in the bs4 and gives it the name/alias bs
-import pandas as pd, numpy as np    #^
+from bs4 import BeautifulSoup as bs         # searches and loads BeautifulSoup in the bs4 and gives it the name(alias) "bs"
+import pandas as pd, numpy as np            #^
 
 print('Module Import = Done \n \n Start the next bit') #\n will create a new line in the output. Like hitting the enter key
 
 #part ONE (makes hashtag + opens instagram )
 
-hashtag = 'DCDog' # This assigns the #hashtag that will be searched for in Instagram
+hashtag = 'dc' # This assigns the #hashtag that will be searched for in Instagram
 print('Looking for >#',hashtag+"<") # Variabales can be included in print functions like this. remember to seperate the pieces of your
                                    # Print function with commas , or plus + signs
 browser = webdriver.Chrome(r'C:\Users\19258\Desktop\chromedriver.exe') # Finds the webdriver.exe on the path provided
                                                                        # Selenium requires a driver to interface with the chosen browser
                                                                        # All webdrivers can be found at: https://docs.seleniumhq.org/download/
                                                                        # You will need to download the corresponding webdriver for the browser you want to use
-browser.get('https://www.instagram.com/explore/tags/' + hashtag) # Load the page at the given url with the #Hashtag
+browser.get('https://www.instagram.com/explore/tags/' +hashtag) # Load the page at the given url with the #Hashtag
 print("Here is the browser",browser)
 Pagelength = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Scrolls to the ZERO position (horizontal)
                                                                                        # Document.body.scrollHeight gets the height
@@ -51,6 +51,7 @@ for i in range(len(links)): # Iterates from zero to the length of the list list
         data = bs(page, 'html.parser')    # Uses the BeautifulSoup html parser to load the 'page' url as soup data to  'data'
         body = data.find('body')          # Finds the <body> tag in the soup and puts it into 'body'
         script = body.find('script')      # Finds the <script...> and puts it into 'script'
+        # print(script)
         raw = script.text.strip().replace('window._sharedData =', '').replace(';', '')
         # print("this is raw! ",raw)
         json_data=json.loads(raw)
@@ -67,11 +68,10 @@ result = result.drop_duplicates(subset = 'shortcode')
 result.index = range(len(result.index))
 print('Part2 is complete')
 
-#part THREE:
+#part THREE: This is the part where the images are loaded on your computah
 
 import os
 import requests
-i = 0
 result.index = range(len(result.index))
 directory= r"C:\Users\19258\Desktop\code\python_test_code\scrape\images\new"
 
